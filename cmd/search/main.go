e62db2e92a1a04664dc07e7a5cd4446687f3706a
Chris Killpack <chris@chriskillpack.com>
2025-01-02 12:32:56 -0800

    Start of a web interface for searching

--- cmd/search/main.go
+++ cmd/search/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
 
 	"github.com/chriskillpack/column"
 )
 
-var flagIndexDir = flag.String("indexdir", "out/", "Directory that holds the search index")
+var (
+	flagIndexDir = flag.String("indexdir", "out/", "Directory that holds the search index")
+	flagQuery    = flag.String("query", "", "query index, print results, quit")
+)
 
 func main() {
 	flag.Parse()
@@ -17,11 +26,36 @@ func main() {
 		log.Fatal(err)
 	}
 
-	query := "forecast"
-	if len(flag.Args()) > 0 {
-		query = flag.Arg(0)
+	if *flagQuery != "" {
+		idx.FindWord(*flagQuery)
+		idx.Finish()
+		os.Exit(0)
 	}
-	idx.FindWord(query)
 
-	idx.Finish()
+	// Start webserver
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	srv := NewServer("8080")
+	go func() {
+		if err := srv.Start(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed to start: %s", err)
+		}
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+
+		shutdownCtx := context.Background()
+		shutdownCtx, cancel := context.WithTimeout(shutdownCtx, 10*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			log.Printf("Error at server shutdown: %s", err)
+		}
+	}()
+	wg.Wait()
 }
